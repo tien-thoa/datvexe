54acf2233ec588d6bf887f8aa66008fdd97436bd
Hoang <dhuyhoang3107@gmail.com>
2021-03-11 11:40:56 +0700

    edit register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -108,7 +108,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-primary" style="margin-left: 70px;">
                                     {{ __('Register') }}
                                 </button>
                             </div>
